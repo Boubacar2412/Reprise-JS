d547fe71b86e344253603f594c9b115d69c0bba1
amad9720 <amly212782@gmail.com>
2024-09-16 10:42:20 +0000

    string course

--- Module JS/cours/script_string.js
+++ Module JS/cours/script_string.js
@@ -59,3 +59,75 @@ console.log(announcement.replace(/door/g, "gate"));
 console.log(announcement.includes("door"));
 console.log(announcement.startsWith("Al"));
 console.log(announcement.endsWith("23!"));
+
+console.log("a+very+nice+day".split("+"));
+console.log("Amadou Ly".split(" "));
+
+const [firstName, lastName] = "Amadou Ly".split(" ");
+
+console.log(firstName);
+console.log(lastName);
+
+`${firstName} ${lastName}`;
+
+const newName = ["Mr", "Amadou", "Ly".toUpperCase()].join(" ");
+console.log(newName);
+
+const passenger = "n'dilamo ousmane samory toure";
+
+const capitalizeName = function (name) {
+  const chunks = name.split(" ");
+  const chunksUpper = [];
+
+  for (const chunk of chunks) {
+    chunksUpper.push(chunk[0].toUpperCase() + chunk.slice(1));
+  }
+
+  return chunksUpper;
+};
+
+const normalizedName = capitalizeName(passenger).join(" ");
+console.log(normalizedName);
+
+//Padding
+const message = "Go to gate 23!";
+
+console.log(message.padStart(2, "+").padEnd(2, "+"));
+// console.log(message.padEnd(25, "+_*"));
+
+const maskCreditCard = function (number) {
+  const str = number + "";
+  const last4Digit = str.slice(-4);
+  const maskedPart = last4Digit.padStart(str.length, "*");
+
+  // Add spaces every 4 characters
+  // return maskedPart.replace(/(.{4})/g, "$1 ").trim();
+
+  let result = "";
+
+  for (let i = 0; i < maskedPart.length; i++) {
+    if (i > 0 && i % 4 === 0) {
+      result += " ";
+    }
+    result += maskedPart[i];
+  }
+
+  return result;
+};
+
+maskCreditCard(4337876386487348);
+console.log(maskCreditCard("76386487348"));
+
+//Repeat
+const message2 = "Bad weather...All departure delayed... ";
+
+console.log(message2.repeat(5));
+
+const planesInline = function (n) {
+  console.log(`There are  ${n} [planes in line ${"✈️".repeat(n)}]`);
+};
+
+planesInline(5);
+planesInline(4);
+planesInline(3);
+planesInline(1);
