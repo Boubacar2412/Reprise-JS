8bbaaff5395a2b92ca64029cae2c7ac039b296d5
amad9720 <amly212782@gmail.com>
2024-10-03 10:40:19 +0000

    projet backecodom - attributes, styles and classes in js

---
+++ Module JS/projets/bankeco-dom/script_starter.js
@@ -0,0 +1,105 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btnOpen => btnOpen.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+// Selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+
+console.log(allButtons);
+console.log(allSections);
+
+const allButtons2 = document.getElementsByClassName('btn');
+
+// Creating and inserting elements
+// insertAdjacentHTML
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookie for improved functionnality and analytics';
+message.innerHTML = `
+We use cookie for improved functionnality and analytics
+<button class="btn btn--close-cookie" >Got it!</button>
+`;
+
+header.append(message);
+header.prepend(message);
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.width);
+
+console.log(getComputedStyle(message).height);
+
+const messageStyleHeight = getComputedStyle(message).height;
+message.style.height = Number.parseFloat(messageStyleHeight) + 40 + 'px';
+
+console.log(message.style.height);
+
+// document.documentElement.style.setProperty('width', 'orange');
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.ariaHidden);
+console.log(logo.className);
+
+logo.alt = 'Beautiful blue logo';
+
+// console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+console.log(logo.setAttribute('designer', 'ousname'));
+console.log(logo.setAttribute('company', 'ecobank'));
+
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data attribute
+console.log(logo.dataset.versionNumber);
+
+//classes
+logo.classList.add('a', 'b', 'c');
+logo.classList.remove('a');
+logo.classList.toggle('b');
+logo.classList.contains('b');
+
+console.log(logo.className);
