cb55b6b4acbbe6381e436cb47d7e493a7b44179f
amad9720 <amly212782@gmail.com>
2024-10-07 11:47:59 +0000

    tabs, scrolling, sticky

--- Module JS/projets/bankeco-dom/script_starter.js
+++ Module JS/projets/bankeco-dom/script_starter.js
@@ -8,6 +8,14 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+// Links
+const nav = document.querySelector('.nav');
+
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -39,15 +47,6 @@ btnScrollTo.addEventListener('click', function (e) {
 });
 
 // Page navigation
-
-// document.querySelectorAll('.nav__link').forEach(function (el) {
-//   el.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     const id = this.getAttribute('href');
-//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-//   });
-// });
-
 // 1. Add an event listener to a common parent element
 // 2. Determine what element originated the event
 
@@ -74,3 +73,76 @@ console.log(allButtons);
 console.log(allSections);
 
 const allButtons2 = document.getElementsByClassName('btn');
+
+const h1 = document.querySelector('h1');
+
+// Downward : child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+console.log(h1.firstChild.parentNode);
+console.log(h1.lastChild);
+
+// h1.firstElementChild.style.color = 'white';
+
+// UpWard : parent
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+// h1.closest('header').style.background = 'var(--color-primary)';
+
+// h1.closest('h1').style.background = 'var(--color-secondary)';
+
+// Selecting siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.nextSibling);
+console.log(h1.previousSibling);
+
+console.log(h1.parentElement.children);
+
+// [...h1.parentElement.children].forEach(function (el) {
+//   if (el !== h1) el.style.transform = 'scale(1.5)';
+// });
+
+// tabs.forEach(t => t.addEventListener('click', () => console.log('TAB')));
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  if (!clicked) return;
+
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(tc => tc.classList.remove('operations__content--active'));
+
+  clicked.classList.add('operations__tab--active');
+
+  // Activation of content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+// Menu fade animations
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+  }
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+// Sticky navigation
+const initialCoords = section1.getBoundingClientRect();
+
+window.addEventListener('scroll', function (e) {
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
