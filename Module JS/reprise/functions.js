c813f91a563cfab22695c0106b4e507821ba912f
Boubacar2412 <hawa567806@gmail.com>
2025-04-21 16:40:23 +0000

    Révision_sur_les_fonctions

---
+++ Module JS/reprise/functions.js
@@ -0,0 +1,20 @@
+/*function multiplicaion (x,y){
+    z = x * y;
+    return x * y;
+}
+
+let resultat = multiplicaion(10,8);
+
+alert(resultat);*/
+
+
+function affichage(){
+    return alert("je suis un développeur js.")
+}
+
+let recursion = affichage();
+
+alert(affichage());
+alert(recursion);
+
+
