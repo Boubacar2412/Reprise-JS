cdb5702272016832be7f0cb4556f42adc9c6014f
Boubacar2412 <hawa567806@gmail.com>
2025-04-22 23:02:01 +0000

    création et manipulation des tableaux

---
+++ Module JS/reprise/arrays.js
@@ -0,0 +1,21 @@
+
+
+let tab = ["bouba", "diallo", 22];
+
+let tab1 = new Array("bouba", "diallo", 22);
+
+alert(tab1[2]);
+
+tab[8] = "foot";
+
+alert(tab[8]);
+
+alert(tab.length);
+
+let p = " ";
+
+for(var i=0; i<tab1.length; i++){
+    p+= "position numero" + (i+1) + " : " + tab1[i] + "\n";
+}
+alert(p);
+
