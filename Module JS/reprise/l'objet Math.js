4726ffa4fb782d91796d79be5ac0f8c8c1148582
Boubacar2412 <hawa567806@gmail.com>
2025-04-23 12:45:30 +0000

    methodes de l'objet Math

--- Module JS/reprise/l'objet Math.js
+++ Module JS/reprise/l'objet Math.js
@@ -1,11 +1,17 @@
-
+// découverte de l'objet Math
 let x = (Math.random()) * 100;
 
 
 let y = Math.round(x);
 
-
-
 alert(x);
 
 alert(y);
+
+
+let inf = Math.floor(x);
+let sup = Math.ceil(x);
+
+let t = Math.trunc(x);
+
+alert(inf); alert(sup); alert(t);
