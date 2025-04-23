901f7edca6dee358d348efcfd1fa96d20d28ca68
Boubacar2412 <hawa567806@gmail.com>
2025-04-23 17:32:39 +0000

    des exemples sur la notion de closures

--- Module JS/reprise/closures.js
+++ Module JS/reprise/closures.js
@@ -1,2 +1,45 @@
+// Notion de closure
+function compteur() {
+    let count = 0;
+  
+    return function() {
+      count++;
+      console.log(count);
+    };
+  }
+  
+  let incrementer = compteur();
+  incrementer();
+  incrementer();
+  incrementer();
+  
+  let autre = compteur();
+  autre();
+  incrementer();
+  
 
 
+  function exemple1() {
+    let x = 42;
+  
+    return function() {
+      console.log("Hello");
+    };
+  }
+  
+  let f = exemple1();
+  f(); // 🔁 "Hello"
+  // ❌ Pas de closure sur "x" car la fonction interne ne l’utilise pas
+
+  
+  function exemple2() {
+    let x = 42;
+  
+    return function() {
+      console.log(x); // 👈 x est utilisée ici
+    };
+  }
+  
+  let f2 = exemple2();
+  f2(); // ✅ "42" – la closure fonctionne
+  
