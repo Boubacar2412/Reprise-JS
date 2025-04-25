ea583f9c8030842c8b44b295b0dd0eb1e82466b4
Boubacar2412 <hawa567806@gmail.com>
2025-04-25 10:55:08 +0000

    Définition du DOM

---
+++ Module JS/reprise/DOM.js
@@ -0,0 +1,11 @@
+
+/*
+Le DOM (Document Object Model) est une interface de 
+programmation qui permet aux langages comme JavaScript
+ d'accéder et de manipuler les éléments HTML et CSS d'une page web. 
+ Lorsqu'une page est chargée, le navigateur crée automatiquement 
+ un objet appelé document qui représente toute la structure de la page.
+  Grâce à cet objet, on peut utiliser différentes méthodes et propriétés
+   JavaScript pour rendre la page dynamique : modifier du texte, ajouter ou supprimer des éléments, 
+   changer des styles, réagir à des événements, etc.
+*/
